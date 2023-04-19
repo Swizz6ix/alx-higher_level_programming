d01c3c3b9582d659e63d90a00d209f9a8adbe3ca
swizz6ix <ferdinandcharles6@gmail.com>
2023-04-18 19:41:25 -0700

    sql

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Displays all values in the states table of the database hbtn_0e_0_usa
+"""
+Displays all values in the states table of the database hbtn_0e_0_usa
  whose name matches that supplied as argument.
  Safe from SQL injections.
  Usage: ./3-my_safe_filter_states.py <mysql username> \
@@ -7,7 +8,6 @@
                                      <database name> \
                                      <state name searched>
 """
-
 import sys
 import MySQLdb
 
